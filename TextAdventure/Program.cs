82cf31cf499be0f7d30639214c5e7148b13a6945
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-22 23:51:12 -0500

    Adding user ability to speak or run... changes are still needed

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -17,15 +17,8 @@ namespace TextAdventure
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("WARNING!");
                 Console.WriteLine(" ");
-
-                //while (i == 2)
-                //{
-                //    Console.ForegroundColor = ConsoleColor.White;
-                //    Console.WriteLine("Help");
-
-                //}
-                //Console.Clear();
             }
+
             Console.WriteLine(" ");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("...coming from the radio.");
@@ -33,43 +26,34 @@ namespace TextAdventure
 
             Console.WriteLine(" ");
             System.Threading.Thread.Sleep(2000);
-                Console.WriteLine("You pull the car to a stop and you glance over at your friends ");
-                Console.WriteLine("only to find them frozen in a daze");
-                Console.WriteLine(" ");
-
-                string response;
-                Console.WriteLine("Will you LOOK or will you RUN first?");
-                response = Console.ReadLine();
-
-    
-                if(response == "LOOK")
-                {
-                    Console.WriteLine(" Something Happens ");
-                }
-                else if
-                {
-                    Console.WriteLine("something else happens");
-                }
-                 else
-                {
-                Console.WriteLine("You must make a choise");
-                }
-            
-
-                      
+            Console.WriteLine("You pull the car to a stop and you glance over at your friends ");
+            Console.WriteLine("only to find them hypnotized by a glowing light");
+            Console.ReadKey();
+            Console.WriteLine(" ");
 
 
+            string speakOrRunResponse;
+            //bool responded = true;
+            Console.WriteLine("Their fate is in your hands!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Will you try to SPEAK to the light or will you RUN?");
+            speakOrRunResponse = Console.ReadLine();
 
-
-
-
-
-
-
-            //Console.WriteLine("C#, Git, GitHub, Visual Studio!");
-            //Console.WriteLine("Are you intered in coding .. see if it is for you. What are you waiting for? ..Take our mini intro to coding and YOU get to decide!");
-            //Console.WriteLine(" ");
-
-        }
+            Console.ForegroundColor = ConsoleColor.White;
+            while (speakOrRunResponse != "SPEAK" && speakOrRunResponse != "RUN")
+            {
+                //responded = false;
+                Console.WriteLine("Quick! You have to make a choice!");
+                break;
+            }
+            if (speakOrRunResponse == "SPEAK")
+            {
+                Console.WriteLine(" Something Happens ");
+            }
+            else if (speakOrRunResponse == "Run")
+            {
+                Console.WriteLine("something else happens");
+            }
+        }   
     }
 }
